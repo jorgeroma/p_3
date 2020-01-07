73e33b6a96607ab1a8fe4d05d1e8ba207a6bba23
jorgeroma <jorgeroma2000@gmail.com>
2020-01-07 01:05:16 +0100

    codigo innecesario

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -179,58 +179,6 @@ class PeliculaDetalle extends StatelessWidget {
     );
   }
 
-
-
-
-
-
-  Future<void> _downloadImage(String url, {AndroidDestinationType destination, bool whenError = false}) async {
-    String fileName;
-    String path;
-    int size;
-    String mimeType;
-    try {
-      String imageId;
-
-      if (whenError) {
-        imageId = await ImageDownloader.downloadImage(url).catchError((error) {
-          if (error is PlatformException) {
-            var path = "";
-            if (error.code == "404") {
-              print("Not Found Error.");
-            } else if (error.code == "unsupported_file") {
-              print("UnSupported FIle Error.");
-              path = error.details["unsupported_file_path"];
-            }
-          }
-
-          print(error);
-        }).timeout(Duration(seconds: 10), onTimeout: () {
-          print("timeout");
-        });
-      } else {
-        if (destination == null) {
-          imageId = await ImageDownloader.downloadImage(url);
-        } else {
-          imageId = await ImageDownloader.downloadImage(
-            url,
-            destination: destination,
-          );
-        }
-      }
-
-      if (imageId == null) {
-        return;
-      }
-      fileName = await ImageDownloader.findName(imageId);
-      path = await ImageDownloader.findPath(imageId);
-      size = await ImageDownloader.findByteSize(imageId);
-      mimeType = await ImageDownloader.findMimeType(imageId);
-    } on PlatformException catch (error) {
-      return;
-    }
-  }
-
   Future<void> _descargar(String url, String nombre) async {
     try {
       // Saved with this method.
@@ -247,7 +195,5 @@ class PeliculaDetalle extends StatelessWidget {
       print(error);
     }
   }
-
-
-
+  
 }
